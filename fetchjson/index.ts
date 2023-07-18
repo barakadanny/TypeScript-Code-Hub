47dc89d5e7c25e1b60ee7197d8d8efed3802e38c
Danny <barakadan421@gmail.com>
2023-07-19 02:17:16 +0300

    Add logTodo function

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -11,13 +11,17 @@ interface Todo {
 axios.get(url).then((response) => {
   const todo = response.data as Todo;
 
-  const ID = todo.id;
+  const id = todo.id;
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
-    The Todo with ID: ${ID}
+    The Todo with ID: ${id}
     Has a title of: ${title} 
     Is it finished? ${completed}
   `);
-});
+};
