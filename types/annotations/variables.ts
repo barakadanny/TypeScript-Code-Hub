2a8738ab00515e6a860b8a849ac202563a394a34
Danny <barakadan421@gmail.com>
2023-07-20 15:50:42 +0300

    variables whose type cannot be inferred correctly

--- types/annotations/variables.ts
+++ types/annotations/variables.ts
@@ -38,3 +38,13 @@ for (let i = 0; i < words.length; i++) {
     foundWords = true;
   }
 }
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
