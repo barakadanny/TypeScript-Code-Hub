e2ba714aef05ee07efd22115c591d000fa6c4758
Danny <barakadan421@gmail.com>
2023-07-20 14:20:53 +0300

    Array & classes

---
+++ types/annotations/variables.ts
@@ -0,0 +1,12 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = false;
+
+// Array
+let colors: string[] = ["red", "green", "yellow"];
+let myNumbers: number[] = [1, 3, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
