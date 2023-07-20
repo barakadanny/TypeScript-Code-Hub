01df8072cbd93d6e8aa1fe3326964a57e7576fdb
Danny <barakadan421@gmail.com>
2023-07-20 15:37:32 +0300

    function

--- types/annotations/variables.ts
+++ types/annotations/variables.ts
@@ -27,3 +27,14 @@ const logNumber: (i: number) => void = (i: number) => {
 // 1) Function that returns the 'any' type
 const json = '{"x": 10, "y": 20}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
+
+// 2) When we declare a variable on one line
+// and initialize it later.
+let words = ["red", "green", "blue"];
+let foundWords: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWords = true;
+  }
+}
